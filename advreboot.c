e7f1fbe7f5ae9faa66f4c2eb7190630d9060967f
BigfootACA <bigfoot@classfun.cn>
2021-04-16 18:06:47 +0800

    advreboot.c: avoid options

--- advreboot.c
+++ advreboot.c
@@ -3,11 +3,13 @@
 #include<stdio.h>
 #include<sys/syscall.h>
 #include<linux/reboot.h>
+static int usage(int code){
+	fprintf(stderr,"Usage: advreboot <COMMAND>\n");
+	exit(code);
+	return code;
+}
 int main(int argc,char**argv){
-	if(argc!=2){
-		fprintf(stderr,"Usage: advreboot <COMMAND>\n");
-		exit(1);
-	}
+	if(argc!=2||argv[1][0]=='-')return usage(1);
 	sync();
 	return (int)syscall(
 		SYS_reboot,
