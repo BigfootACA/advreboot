f59082e809f094cbb1f9f203bfec33f2c7681f94
BigfootACA <bigfoot@classfun.cn>
2021-05-04 17:57:26 +0800

    advreboot.c: add getopt

--- advreboot.c
+++ advreboot.c
@@ -3,19 +3,36 @@
 #include<stdio.h>
 #include<sys/syscall.h>
 #include<linux/reboot.h>
+#include<getopt.h>
 static int usage(int code){
 	fprintf(stderr,"Usage: advreboot <COMMAND>\n");
 	exit(code);
 	return code;
 }
 int main(int argc,char**argv){
-	if(argc!=2||argv[1][0]=='-')return usage(1);
+	static struct option lo[]={
+		{"help",no_argument,0,'h'},
+		{0,0,0,0}
+	};
+	int o;
+	while((o=getopt_long(argc,argv,"h",lo,NULL))>0)switch(o){
+		case 'h':return usage(0);
+		case '?':return usage(2);
+		default:return 1;
+	}
+	if(argc<=optind){
+		fprintf(stderr,"advreboot: missing command\n");
+		return 2;
+	}else if(argc>optind+1){
+		fprintf(stderr,"advreboot: too many arguments\n");
+		return 2;
+	}
 	sync();
 	return (int)syscall(
 		SYS_reboot,
 		LINUX_REBOOT_MAGIC1,
 		LINUX_REBOOT_MAGIC2,
 		LINUX_REBOOT_CMD_RESTART2,
-		argv[1]
+		argv[optind]
 	);
 }
